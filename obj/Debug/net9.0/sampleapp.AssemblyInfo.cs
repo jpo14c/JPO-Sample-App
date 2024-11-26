034e072500655002ed9a1ec2068e391f60b451a0
Juan Ospina <156124479+jpo14c@users.noreply.github.com>
2024-11-26 10:21:34 -0500

    change

--- obj/Debug/net9.0/sampleapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/sampleapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sampleapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5675b7ef9c519f9fe507697f6eed29d9425e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("sampleapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sampleapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
